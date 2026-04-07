d0805dbd5fb74589c87b56a6900e5da09d78efc2
adhitya <sukmaadhitya@gmail.com>
2026-04-07 16:36:18 +0700

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -240,7 +240,7 @@ export default function Home() {
       {/* Footer */}
       <footer className="text-center mt-16 pb-8">
         <p className="text-xs text-white/20">
-          Social Media Views Dashboard • Adhitya Sukma
+          Social Media Views Dashboard • Created by Adhitya Sukma
         </p>
       </footer>
     </main>
